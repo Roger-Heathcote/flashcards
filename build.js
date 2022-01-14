b3069501bc785e94570f5a7bf73900dd029aacfe
Roger-Heathcote <github@technicalbloke.com>
2022-01-14 19:23:06 +0000

    Made into an NPM package

---
+++ build.js
@@ -0,0 +1,1 @@
+console.log("Some day this script will build the .md files in ./cards into files that can be imported into Anki, and/or other flashcard formats. Til then it assumes you are on some form of unix and have mdankideck installed. Run `npm run build` to build the Anki decks.")
